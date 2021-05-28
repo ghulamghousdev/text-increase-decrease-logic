207d78f01e4c21f860022880b45da87aa7ad183b
ghulamghousdev <itsghulamghous@gmail.com>
2021-05-28 11:28:54 +0500

    First Commit

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import logo from "./logo.svg";
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import "./AccessibleFonts.css";
+
+function App() {
+  const [sliderValue, setSliderValue] = useState(10);
+  const [fontType, setFontType] = useState("Small");
+  const setFont = () => {
+    if (sliderValue === 10) {
+      setFontType("Small");
+    } else if (sliderValue === 20) {
+      setFontType("Medium");
+    } else if (sliderValue === 30) {
+      setFontType("SmallMedium");
+    } else if (sliderValue === 40) {
+      setFontType("Large");
+    }
+  };
+  const chng = (e) => {
+    let a = sliderValue;
+    a = a + 10;
+    setSliderValue(a);
+    setFont();
+  };
+
+  return (
+    <div className="App">
+      <button onClick={(e) => chng(e)}>increaseSize</button>
+      <button>decreaseSize</button>
+      <p className={`header${fontType}`}>
+        Some text to have its text adjusted by the buttons just up there.
+      </p>
+
+      <blockquote className={`footer${fontType}`}>
+        Some text in a blockquote
+      </blockquote>
+    </div>
+  );
+}
+
+export default App;
